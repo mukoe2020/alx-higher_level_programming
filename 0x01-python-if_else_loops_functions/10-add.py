324177c5c2caf3f7a8d0583b8125ba4128c8de03
memoe <memoemukoe@gmail.com>
2023-08-08 04:02:12 -0700

    function that adds two integers and returns the result

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    return a + b
+
+
+def main():
+    print(add(1, 2))
+    print(add(98, 0))
+    print(add(100, -2))
+
+
+if __name__ == "__main__":
+    main()
