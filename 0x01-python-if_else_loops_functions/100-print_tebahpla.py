3807f99a68e72fad20e4dff3ccc524b6cbe8e84d
MemoeMukoe <122824378+mukoe2020@users.noreply.github.com>
2023-08-08 14:46:24 +0200

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 for i in range(25, -1, -1):
-    print("{:c}".format(ord('Z') - i), end="")
     print("{:c}".format(ord('z') - i), end="")
+    print("{:c}".format(ord('Z') - i), end=""):wq
+    
