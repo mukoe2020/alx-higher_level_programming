c8ac2c9a23bb0697f9872858de2426a8240fd8bf
memoe <memoemukoe@gmail.com>
2023-08-08 02:39:47 -0700

    asscii alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+output = ""
 for i in range(ord('a'), ord('z') + 1):
-    print(chr(i), end='')
+    if chr(i) not in ['q', 'e']:
+        output += chr(i)
+
+print("{}".format(output), end='')
