d9b11fa178af184d43815d0538248e51a42d6eda
memoe <memoemukoe@gmail.com>
2023-08-10 02:39:36 -0700

    a program that imports functions from the file

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+result_add = add(a, b)
+result_sub = sub(a, b)
+result_mul = mul(a, b)
+result_div = div(a, b)
+
+print("{0} + {1} = {2}".format(a, b, result_add))
+print("{0} - {1} = {2}".format(a, b, result_sub))
+print("{0} * {1} = {2}".format(a, b, result_mul))
+print("{0} / {1} = {2}".format(a, b, result_div))
