50fcce5b7dfa28928c2c6807cf03b69cf93ca1f1
memoe <memoemukoe@gmail.com>
2023-08-11 01:37:26 -0700

    My first toolbox!

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     # Your program should call each of the imported functions.
-
     from calculator_1 import add, sub, mul, div
-# a and b must be defined in 2 different lines: a = 10 and another b = 5
 
-a = 10
-b = 5
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    # a and b must be defined in 2 different lines: a = 10 and another b = 5
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
