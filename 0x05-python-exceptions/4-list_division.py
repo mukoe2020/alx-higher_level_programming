48d9796cceb3708a5bd329a209bc5f502ed56c54
memory mukonda <memoemukoe@gmail.com>
2023-08-28 15:32:52 +0200

    raise exeception

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-
     for i in range(list_length):
         try:
-            elem_1 = my_list_1[i] if i < len(my_list_1) else 0
-            elem_2 = my_list_2[i] if i < len(my_list_2) else 1
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                raise IndexError("out of range")
 
-            if type(elem_1) not in (int, float) or type(elem_2) not in (int, float):
-                print("wrong type")
-                result.append(0)
-            elif elem_2 == 0:
-                print("division by 0")
-                result.append(0)
-            else:
-                result.append(elem_1 / elem_2)
+            elem_1 = my_list_1[i]
+            elem_2 = my_list_2[i]
+
+            if not isinstance(elem_1, (int, float)) or not isinstance(elem_2, (int, float)):
+                raise TypeError("wrong type")
+
+            if elem_2 == 0:
+                raise ZeroDivisionError("division by 0")
+
+            division_result = elem_1 / elem_2
+            result.append(division_result)
         except IndexError:
             print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
             result.append(0)
         except ZeroDivisionError:
             print("division by 0")
             result.append(0)
-
+        finally:
+            pass
     return result
