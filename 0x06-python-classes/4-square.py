e0196196cd23443a3b7544f0a09aae51719140d1
memory mukonda <memoemukoe@gmail.com>
2023-08-30 12:27:07 +0200

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,21 @@
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size attribute with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size * self.__size
