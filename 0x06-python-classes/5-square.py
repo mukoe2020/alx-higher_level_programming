9c96e8f6f18322717a1542744f2a1776eaaf15a9
memory mukonda <memoemukoe@gmail.com>
2023-08-30 12:37:27 +0200

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve the size attribute."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size attribute with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Print the square using '#' character."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+# Example usage
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 10
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 0
+    my_square.my_print()
+
+    print("--")
