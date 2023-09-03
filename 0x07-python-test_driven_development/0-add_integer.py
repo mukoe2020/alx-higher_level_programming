d140220dfca4f63ddfff439d52d5ed703cc49090
memory mukonda <memoemukoe@gmail.com>
2023-09-03 19:32:04 +0200

    interger addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number. Defaults to 98.
+
+    Returns:
+        int: The sum of the two numbers (a + b).
+
+    Raises:
+        TypeError: If a or b is not an integer or a float.
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
