1d55af1476b9fd08fd351245de5797730974fc8e
memory mukonda <memoemukoe@gmail.com>
2023-09-04 17:40:51 +0200

    rectangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -3,9 +3,11 @@
 this a module to print an empty class of a rectangle
 
 """
-class rectangle: 
-    """                                                                         
-      thats the rectangle class 
-    """                                                                        
-    def __init__(self):                                                         
-     pass   
+
+
+class rectangle:
+    """
+    thats the rectangle class
+
+    """
+    pass
