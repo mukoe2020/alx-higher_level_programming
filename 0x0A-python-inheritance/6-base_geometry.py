6bb473ca9bbe32ddab76105279394fae890eba9c
memoe <memoemukoe@gmail.com>
2023-09-12 04:58:54 -0700

    Improve Geometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry"""
+"""Defining class BaseGeometry"""
 
 
 class BaseGeometry:
-    """this class represents a base geometry"""
-
+    """Initializing class"""
     def area(self):
-        """method not implemented yet"""
+        """rasing an exception"""
         raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """validates a value as an integer
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
