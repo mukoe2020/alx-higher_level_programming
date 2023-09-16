b3fe58dca7c21149da1a4616bb0eaa8336f6f08d
memoe <memoemukoe@gmail.com>
2023-09-16 11:11:21 -0700

    And now, the Square!

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,41 @@
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from the Rectangle class.
+    Represents a square shape with equal width and height.
+
+    Attributes:
+    size (int): The size (width/height) of the square.
+    x (int): The x-coordinate of the square's position.
+    y (int): The y-coordinate of the square's position.
+    id (int): The unique identifies the square inherited from Rectangle class.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square instance
+
+        Raises:
+        TypeError: If size, x, or y is not an integer.
+        ValueError: If size, x, or y is less than 0.
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get for the size attribute."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set for the size attribute."""
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """
+        Returns a formatted string representation of the square
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
