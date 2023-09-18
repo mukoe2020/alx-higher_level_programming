76a9d3f1d691b4007c9f0f44a44ab21b3fdd926d
MemoeMukoe <122824378+mukoe2020@users.noreply.github.com>
2023-09-18 21:10:08 +0200

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
+"""Defining a class square, inheriting from the class rectangle"""
 from models.rectangle import Rectangle
-"""
-importing square module
-"""
-
 
 class Square(Rectangle):
     """
