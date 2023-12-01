d79d8b4bb94ac07e5aca101b1ed994e446126384
memoe <memoemukoe@gmail.com>
2023-12-01 16:18:57 +0200

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id
+a Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id
 """
 
 import sys
